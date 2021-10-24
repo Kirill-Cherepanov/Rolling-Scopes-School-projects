32970b245fd3b1042d6bec287f60a07ed864ac35
Kirill Cherepanov <cherkirr@gmail.com>
2021-10-24 21:59:37 +0300

    feat: add hide blocks in settings

--- momentum/js/settings.js
+++ momentum/js/settings.js
@@ -4,18 +4,22 @@ const langSelect = document.getElementById('lang');
 const imgSource = document.querySelectorAll('input[name="img-src"]');
 const tagInput = document.getElementById('add-tag');
 const tagsField = document.querySelector('.tags-field');
+const blockSelect = document.getElementById('hide-blocks');
+const hiddenBlocks = JSON.parse(localStorage.getItem('hiddenBlocks')) || [];
 let tags = JSON.parse(localStorage.getItem('imgtags')) || [];
 
+// Toggle menu
 settingsBtn.addEventListener('pointerdown', () => settingsForm.classList.toggle('active'));
 
-langSelect.addEventListener('change', () => {
-  localStorage.setItem('lang', langSelect.value);
-});
+// Language change
+langSelect.addEventListener('change', () => localStorage.setItem('lang', langSelect.value));
+
+// Image source change
 Array.from(imgSource).forEach(i => {
-  i.addEventListener('change', () => {
-    localStorage.setItem('imgsrc', i.id);
-  });
+  i.addEventListener('change', () => localStorage.setItem('imgsrc', i.id));
 });
+
+// Tagfield and tags search change
 const addTag = function(text) {
   const newTag = document.createElement('div');
   newTag.classList.add('tag');
@@ -34,17 +38,38 @@ tagInput.addEventListener('change', () => {
   localStorage.setItem('imgtags', JSON.stringify(tags));
 });
 
+// Hide / Show blocks
+blockSelect.addEventListener('change', () => {
+  const hiddenBlocks = Array.from(blockSelect.children).filter(i => i.selected).map(i => i.value);
+
+  localStorage.setItem('hiddenBlocks', JSON.stringify(hiddenBlocks));
+});
+
+// Set default local storage values
 if (!localStorage.getItem('lang')) localStorage.setItem('lang', 'en');
 if (!localStorage.getItem('imgsrc')) localStorage.setItem('imgsrc', 'github');
 
-// Set init values
+// Set initial values
 langSelect.value = localStorage.getItem('lang');
 Array.from(imgSource).forEach(i => { 
-  if (i.id === localStorage.getItem('imgsrc')) {
-    i.checked = true; 
-  }
+  if (i.id === localStorage.getItem('imgsrc')) i.checked = true;
 });
 for (t of tags) addTag(t);
+if (hiddenBlocks.includes('time-date')) {
+  document.querySelector(`time.time`).style.opacity = '0';
+  document.querySelector(`time.time`).style.pointerEvents = 'none';
+  document.querySelector(`date.date`).style.opacity = '0';
+  document.querySelector(`date.date`).style.pointerEvents = 'none';
+}
+for (b of hiddenBlocks) {
+  Array.from(blockSelect.children).filter(i => i.value === b)[0].selected = true;
+  if (b != 'time-date') {
+    document.querySelector(`.${b}`).style.opacity = '0';
+    document.querySelector(`.${b}`).style.pointerEvents = 'none';
+  }
+}
+
+// Set a variable for Global Settings
 const globalSettings = {
   language: localStorage.getItem('lang'),
   imgsrc: localStorage.getItem('imgsrc'),
