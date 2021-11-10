d0745bf2c598505a6ce9f5b95b2b308d60b6cee9
Kirill Cherepanov <cherkirr@gmail.com>
2021-11-10 17:42:09 +0300

    feat: add mechanics for most of the project

--- ArtQuiz/js/questions.js
+++ ArtQuiz/js/questions.js
@@ -1,13 +1,11 @@
+import images from './images.js';
 Array.prototype.shuffle = function() {
   return this.sort(() => Math.random() - 0.5);
 };
 
-import images from './images.js';
-
-const timer = document.querySelector('.timer');
-const questions = document.querySelectorAll('p.question');
 
 // Timer handler
+const timer = document.querySelector('.timer');
 const timerTime = localStorage.getItem('timer');
 let timerHandler;
 
@@ -30,23 +28,26 @@ const setTimer = function() {
 };
 
 
-// Quiz setup
 
-// Artists quiz
+// Quiz setup
 const artistsQuiz = document.querySelector('.artists-quiz');
 const qImg = document.querySelector('.q-img');
 let authors = Array.from(document.querySelector('.authors').children);
 
-// Arts quiz
 const artsQuiz = document.querySelector('.arts-quiz');
 const author = document.querySelector('.author');
 let pictures = Array.from(document.querySelector('.pictures').children);
 
-// Common values
-let setQuestion;
+const questions = document.querySelectorAll('p.question');
 const quizType = Number(localStorage.getItem('quiz'));
 const initQuestion = (Number(localStorage.getItem('category')) - 1) * 10;
 let questionNum = initQuestion;
+let setQuestion;
+let answers = JSON.parse(localStorage.getItem('answers')) || {
+  '0': Array(10).fill(Array(11).fill(0)),
+  '1': Array(10).fill(Array(11).fill(0)),
+};
+answers[quizType][initQuestion / 10] = Array(10).fill(0);
 
 if (!quizType) { // Artists
   artsQuiz.style.display = 'none';
@@ -54,28 +55,38 @@ if (!quizType) { // Artists
   setQuestion = function() {
     if (questionNum - initQuestion === 10) {
       window.location.href = 'results.html';
+      answers[quizType][initQuestion / 10][10] = 1;
+      localStorage.setItem('answers', JSON.stringify(answers));
       throw '';
     }
 
-    qImg.src = `assets/img/full/${questionNum}full.jpg`;
+    qImg.src = `./assets/img/full/${questionNum}full.jpg`;
 
     // Set author answers
     authors.shuffle()[0].textContent = images[questionNum].author;
-    authors.slice(1).forEach(author => {
+    authors.slice(1).reduce((usedImgs, author) => {
       let randomNum;
       do { randomNum = Math.floor(Math.random() * 100);
-      } while (randomNum === questionNum);
+      } while (usedImgs.includes(randomNum));
       
       author.textContent = images[randomNum].author;
-    });
+
+      usedImgs.push(randomNum);
+      return usedImgs;
+    }, [questionNum]);
 
     // Event listeners for clicks on answers
     authors.forEach(author => {
       author.addEventListener('pointerdown', function () {
         document.addEventListener('pointerup', function d(e) {
           if (authors.includes(e.target)) {
-            if (authors[0] === e.target) e.target.style.backgroundColor = '#4BB543';
-            else e.target.style.backgroundColor = '#FC100D';
+            if (authors[0] === e.target) {
+              e.target.style.backgroundColor = '#4BB543';
+              answers[quizType][initQuestion / 10][questionNum - initQuestion - 1] = 1;
+            } else {
+              e.target.style.backgroundColor = '#FC100D';
+              answers[quizType][initQuestion / 10][questionNum - initQuestion - 1] = 0;
+            }
 
             // Delete all the Event Listeners on authors and reassign it
             const authorsContainer = document.querySelector('.authors');
@@ -106,27 +117,37 @@ if (!quizType) { // Artists
   setQuestion = function() {
     if (questionNum - initQuestion === 10) {
       window.location.href = 'results.html';
+      answers[quizType][initQuestion / 10][10] = 1;
+      localStorage.setItem('answers', JSON.stringify(answers));
       throw '';
     }
 
     author.textContent = images[questionNum].author;
 
     // Set images answers
-    pictures.shuffle()[0].style.backgroundImage = `url(./assets/img/full/${questionNum}full.jpg)`;
-    pictures.slice(1).forEach(picture => {
+    pictures.shuffle()[0].style.backgroundImage = `url(./assets/img/squared/${questionNum}.jpg)`;
+    pictures.slice(1).reduce((usedNums, picture) => {
       let randomNum;
       do { randomNum = Math.floor(Math.random() * 100);
-      } while (randomNum === questionNum);
-
-      picture.style.backgroundImage = `url(./assets/img/full/${randomNum}full.jpg)`;
-    });
+      } while (usedNums.includes(randomNum));
+      
+      picture.style.backgroundImage = `url(./assets/img/squared/${randomNum}.jpg)`;
+      
+      usedNums.push(randomNum);
+      return usedNums;
+    }, [questionNum]);
 
     // Event listeners for clicks on answers
     pictures.forEach(picture => {
       picture.addEventListener('pointerdown', function () {
         document.addEventListener('pointerup', function d(e) {
-          console.log('fd');
           if (pictures.includes(e.target)) {
+            if (pictures[0] === e.target) {
+              answers[quizType][initQuestion / 10][questionNum - initQuestion - 1] = 1;
+            } else {
+              answers[quizType][initQuestion / 10][questionNum - initQuestion - 1] = 0;
+            }
+
             pictures.slice(1).forEach(picture => {
               picture.style.filter = 'grayscale(100%)';
             });
---
+++ ArtQuiz/js/results.js
@@ -0,0 +1,30 @@
+import images from './images.js';
+
+const answers = JSON.parse(localStorage.getItem('answers')) || Array(10).fill(Array(10).fill(0));
+const picContainer = document.querySelector('.picture-container');
+const imgWrappers = Array.from(picContainer.children);
+const initPicture = (Number(localStorage.getItem('category')) - 1) * 10;
+const quizType = localStorage.getItem('quiz');
+
+for (let i = 0; i < 10; i++) {
+  const tooltip = document.createElement('div');
+  tooltip.classList.add('tooltip');
+
+  let newP = document.createElement('p');
+  newP.textContent = `Автор: ${images[initPicture + i].author}`;
+  tooltip.append(newP);
+
+  newP = document.createElement('p');
+  newP.textContent = `Название: ${images[initPicture + i].name}`;
+  tooltip.append(newP);
+
+  newP = document.createElement('p');
+  newP.textContent = `Год написания: ${images[initPicture + i].year}`;
+  tooltip.append(newP);
+
+  imgWrappers[i].append(tooltip);
+
+  imgWrappers[i].style.backgroundImage = `url(./assets/img/squared/${initPicture + i}.jpg)`;
+  console.log(answers);
+  if (answers[quizType][initPicture / 10][i]) imgWrappers[i].classList.add('correct');
+}
