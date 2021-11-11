dd4ae1527856c09030ffe91a2d4a9d75f52a045f
Kirill Cherepanov <cherkirr@gmail.com>
2021-11-11 10:56:16 +0300

    feat: randomize picture on index.html

--- ArtQuiz/js/index.js
+++ ArtQuiz/js/index.js
@@ -1,5 +1,7 @@
 const artistsBtn = document.querySelector('.Artists');
 const artsBtn = document.querySelector('.Arts');
+const image = document.querySelector('img');
 
 artistsBtn.addEventListener('pointerdown', () => localStorage.setItem('quiz', '0'));
 artsBtn.addEventListener('pointerdown', () => localStorage.setItem('quiz', '1'));
+image.src = `assets//img/squared/${Math.floor(Math.random() * 100 - 1)}.jpg`;
